9f1b0d8277ea3705315541dd33b842e3d0109c2d
Tiago Gomes <tidgomes@gmail.com>
2023-07-04 14:41:12 -0300

    feat: add styling to the InvestmentForm component

--- src/components/InvestmentForm.jsx
+++ src/components/InvestmentForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import CardContainer from './CardContainer'
+import classes from './InvestmentForm.module.css'
 
 const InvestmentForm = () => {
   const [savings, setSavings] = useState(0)
@@ -8,45 +10,51 @@ const InvestmentForm = () => {
   const [period, setPeriod] = useState(0)
 
   return (
-    <Form>
-      <h1>What is your investment plan?</h1>
-      <Form.Group controledId='initialValue'>
-        <Form.Label>Current Savings for Investment</Form.Label>
-        <Form.Control
-          type='number'
-          value={savings}
-          onChange={(event) => setSavings(event.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controledId='yearInvestment'>
-        <Form.Label>Yearly Investment</Form.Label>
-        <Form.Control
-          type='number'
-          value={yearInvestment}
-          onChange={(event) => setYearInvestment(event.target.value)}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Interest per Year</Form.Label>
-        <Form.Control
-          type='number'
-          value={interest}
-          onChange={(event) => setInterest(event.target.value)}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Years</Form.Label>
-        <Form.Control
-          type='number'
-          value={period}
-          onChange={(event) => setPeriod(event.target.value)}
-        />
-      </Form.Group>
-      <Button>Reset</Button>
-      <Button type='submit' variant='primary'>
-        Calculate
-      </Button>
-    </Form>
+    <CardContainer>
+      <Form className='my-3'>
+        <h1>What is your investment plan?</h1>
+        <div className={classes['form-fields']}>
+          <Form.Group className='p-3' controledId='initialValue'>
+            <Form.Label>Current Savings for Investment</Form.Label>
+            <Form.Control
+              type='number'
+              value={savings}
+              onChange={(event) => setSavings(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className='p-3' controledId='yearInvestment'>
+            <Form.Label>Yearly Investment</Form.Label>
+            <Form.Control
+              type='number'
+              value={yearInvestment}
+              onChange={(event) => setYearInvestment(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className='p-3'>
+            <Form.Label>Interest per Year</Form.Label>
+            <Form.Control
+              type='number'
+              value={interest}
+              onChange={(event) => setInterest(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className='p-3'>
+            <Form.Label>Years</Form.Label>
+            <Form.Control
+              type='number'
+              value={period}
+              onChange={(event) => setPeriod(event.target.value)}
+            />
+          </Form.Group>
+        </div>
+        <div className={classes.buttons}>
+          <Button variant='secondary'>Reset</Button>
+          <Button type='submit' variant='primary'>
+            Calculate
+          </Button>
+        </div>
+      </Form>
+    </CardContainer>
   )
 }
 
