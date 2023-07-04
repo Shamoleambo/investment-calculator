0f0396ff720dbe7c74e0174548c13ee44a226b42
Tiago Gomes <tidgomes@gmail.com>
2023-07-04 14:20:55 -0300

    feat: create module.css for CardContainer component

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,7 @@
+import classes from './CardContainer.module.css'
+
 const CardContainer = ({ children }) => {
-  return <div className='cardContainer'>{children}</div>
+  return <div className={classes.cardContainer}>{children}</div>
 }
 
 export default CardContainer
