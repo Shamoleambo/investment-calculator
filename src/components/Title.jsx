96762e88ba1a1eb6ee2e0f7f97b898aff47bbb33
Tiago Gomes <tidgomes@gmail.com>
2023-07-04 14:40:36 -0300

    feat: add styling to the Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,10 +5,12 @@ import classes from './Title.module.css'
 const Title = () => {
   return (
     <CardContainer>
-      <h1>Investment Calculator</h1>
-      <span className={classes.icon}>
-        <BsCash style={{ fontSize: '100px' }} />
-      </span>
+      <div className={classes.pageTitle}>
+        <h1>Investment Calculator</h1>
+        <span className={classes.icon}>
+          <BsCash style={{ fontSize: '100px' }} />
+        </span>
+      </div>
     </CardContainer>
   )
 }
