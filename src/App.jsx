871be26ade9cd32be4f6570cb5f90aa1aa764184
Tiago Gomes <tidgomes@gmail.com>
2023-07-04 14:22:32 -0300

    feat: display Title and InvestmentForm components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Title from './components/Title'
+import InvestmentForm from './components/InvestmentForm'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-  return <div>Investment Calculator</div>
+  return (
+    <>
+      <Title />
+      <InvestmentForm />
+    </>
+  )
 }
 
 export default App
